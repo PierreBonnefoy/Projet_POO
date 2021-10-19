db78d7be0adacf92a1c5bf90d482066961444c31
anthony-guillot <guillot.anthony01@gmail.com>
2021-10-19 17:09:40 +0200

    creation des classes et importation du pdf

---
+++ Wargame/src/wargame/Soldat.java
@@ -0,0 +1,22 @@
+package wargame;
+
+public class Soldat {
+	int getPoints() {
+		
+	}
+	int getTour() {
+		
+	}
+	int getPortee() {
+		
+	}
+	void joueTour(int tour) {
+		
+	}
+	void combat(Soldat soldat) {
+		
+	}
+	void seDeplace(Position newPos) {
+		
+	}
+}
