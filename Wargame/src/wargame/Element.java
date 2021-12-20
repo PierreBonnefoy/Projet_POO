5b98a76e3ad1178377403bf75918db9235ef1c47
2703-web <garnier.soan@gmail.com>
2021-12-20 15:56:32 +0100

    ajout des fichier algo et idee (regle du jeux) + le Element.java mis a jour

--- Wargame/src/wargame/Element.java
+++ Wargame/src/wargame/Element.java
@@ -1,6 +1,28 @@
 package wargame;
 
 public class Element {
-	public Position position;
-	public String type;
+	/*-- Liste des variables --*/
+	/*Element désigne la case du plateau*/
+	private int occupe; /*un entier entre 0 et 3 qui indique l'occupation de la case. Si occupe = 3 : la case est pleine et rien ne peut aller dessus. Lorsqu'une unité va sur une case, il augmente occupe de sa Taille.*/
+	private int nature; /*la liste des ID des personnages qui sont sur la case (il y en as 3 maximum)*/
+	private int[] ids ; /*le type d'élément de terrain de la case (montagne, forêt, pleine,fossé)*/
+	
+	public Element(int n) {
+		this.occupe = 0;
+		this.nature = n;
+		this.ids = new int[3];
+		for(int i = 0 ; i < 3 ; i++) {
+			this.ids[i] = 0;
+		}
+	}
+	
+	public getOccupe() {
+		return Element.occupe;
+	}
+	public setOccupe(int n) {
+		this.occupe = n;
+	}
+	/*le reste est à faire*/
+	
+	
 }
