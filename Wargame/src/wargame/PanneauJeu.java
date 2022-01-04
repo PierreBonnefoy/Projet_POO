6bf251c3e119f61718b848e940c84b35e2fefdb1
2703-web <garnier.soan@gmail.com>
2022-01-04 21:33:32 +0100

    panneauJeu avec boucle de tour incomplete

--- Wargame/src/wargame/PanneauJeu.java
+++ Wargame/src/wargame/PanneauJeu.java
@@ -1,61 +1,156 @@
 package wargame;
 import java.awt.*;
+
 import java.awt.event.*;
 import javax.swing.*;
+
 
 public class PanneauJeu extends JPanel implements IConfig {
 	public static final int taille_fenetre=800;
 	public Carte jeu=new Carte(30,4,2,2);
 	public Position posbuffer=new Position(0,0);
+	JButton bouton4;
 	
 	/*creation des personnages */
-	public Personnage[][] equipe = new Personnage[NBPERSONNAGE][NBJOUEUR];
-	public Personnage abomination = new Personnage(0,1,1,1);
-	public Personnage mutilateur = new Personnage(0,2,1,2);
-	public Personnage incendiaire = new Personnage(0,3,1,3);
-	public Personnage destructeur = new Personnage(0,4,1,4);
-	public Personnage diablotin = new Personnage(0,5,1,5);
-	public Personnage guerrier = new Personnage(0,6,1,6);
-	public Personnage succube = new Personnage(0,7,1,7);
-	public Personnage incube = new Personnage(0,8,1,8);
-	public Personnage cerbere = new Personnage(0,9,1,9);
-	public Personnage oeil = new Personnage(0,10,1,10);
+	public Personnage[][] equipe;
+	public Personnage abomination;
+	public Personnage mutilateur;
+	public Personnage demoniste;
+	public Personnage demon;
+	public Personnage diablotin;
+	public Personnage guerrier;
+	public Personnage succube;
+	public Personnage incube;
+	public Personnage cerbere;
+	public Personnage oeil;
+	public Personnage golem;
+	public Personnage hydre;
+	public Personnage drake;
+	public Personnage minotaure;
+	public Personnage grenouille;
+	public Personnage armure;
+	public Personnage lezard;
+	public Personnage vampyrion;
+	public Personnage predator;
+	public Personnage worm;
 	
 	public PanneauJeu(){
 		this.setPreferredSize(new Dimension(taille_fenetre,taille_fenetre));
 		this.setBackground(Color.black);
 		clickPosition();
 		/*initialisation du tableau d'equipe*/
-		equipe[0][0] = abomination;
-		equipe[1][0] = mutilateur;
-		equipe[2][0] = incendiaire;
-		equipe[3][0] = destructeur;
-		equipe[4][0] = diablotin;
-		equipe[5][0] = guerrier;
-		equipe[6][0] = succube;
-		equipe[7][0] = incube;
-		equipe[8][0] = cerbere;
-		equipe[9][0] = oeil;
+
+		JButton bouton4 = new JButton("Bouton 4");
+		bouton4.setForeground(Color.white);
+		add(bouton4);
+		EcouteurBouton eb = new EcouteurBouton();
+		bouton4.addActionListener(eb);
+		
+		equipe = new Personnage[NBPERSONNAGE][NBJOUEUR];
+		abomination = new Personnage(GARDIEN,IA,1,1);
+		mutilateur = new Personnage(BETE,IA,1,2);
+		demoniste = new Personnage(INCENDIAIRE,IA,1,3);
+		demon = new Personnage(DESTRUCTEUR,IA,1,4);
+		diablotin = new Personnage(ENCHANTEUR,IA,1,5);
+		guerrier = new Personnage(COMBATTANT,IA,1,6);
+		succube = new Personnage(ARTILLEUR,IA,1,7);
+		incube = new Personnage(DANCELAME,IA,1,8);
+		cerbere = new Personnage(CHASSEUR,IA,1,9);
+		oeil = new Personnage(ECLAIREUR,IA,1,10);
+
+		golem = new Personnage(GARDIEN,JOUEUR,2,1);
+		hydre = new Personnage(BETE,JOUEUR,2,2);
+		drake = new Personnage(INCENDIAIRE,JOUEUR,2,3);
+		minotaure = new Personnage(DESTRUCTEUR,JOUEUR,2,4);
+		grenouille = new Personnage(ENCHANTEUR,JOUEUR,2,5);
+		armure = new Personnage(COMBATTANT,JOUEUR,2,6);
+		lezard = new Personnage(ARTILLEUR,JOUEUR,2,7);
+		vampyrion = new Personnage(DANCELAME,JOUEUR,2,8);
+		predator = new Personnage(CHASSEUR,JOUEUR,2,9);
+		worm = new Personnage(ECLAIREUR,JOUEUR,2,10);
+		
+		equipe[0][IA] = abomination;
+		equipe[1][IA] = mutilateur;
+		equipe[2][IA] = demoniste;
+		equipe[3][IA] = demon;
+		equipe[4][IA] = diablotin;
+		equipe[5][IA] = guerrier;
+		equipe[6][IA] = succube;
+		equipe[7][IA] = incube;
+		equipe[8][IA] = cerbere;
+		equipe[9][IA] = oeil;
+		
+		equipe[0][JOUEUR] = golem;
+		equipe[1][JOUEUR] = hydre;
+		equipe[2][JOUEUR] = drake;
+		equipe[3][JOUEUR] = minotaure;
+		equipe[4][JOUEUR] = grenouille;
+		equipe[5][JOUEUR] = armure;
+		equipe[6][JOUEUR] = lezard;
+		equipe[7][JOUEUR] = vampyrion;
+		equipe[8][JOUEUR] = predator;
+		equipe[9][JOUEUR] = worm;
+		
+		for(int i=0;i<NBPERSONNAGE;i++) {
+			for(int j=0;j<NBJOUEUR;j++) {
+				jeu.carte[equipe[i][j].getPos().getX()][equipe[i][j].getPos().getY()].rajoutPersonnage(equipe[i][j]);
+			}
+		}
 	}
 	
-	public void tourdejeux() {
+	public class EcouteurBouton implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			System.out.println("OUII ");
+			
+			repaint();	
+			System.out.println("OUI ");
+			tourDeJeux();
+			System.out.println("oui ");
+			
+		}
+		//NOTE : il est conseillé de faire un ecouteur pour chaque button au lieu de faire des if elses 
+	}
+	
+	public void tourDeJeux() {
 		/*boucle en parcourant le tableau des Personnages, et joue chacun des personnages a tour de rôle*/
 		int indicePerso;
 		int indiceJoueur; 
 		int termine = 0;
-		int[] nb_vivant = new int[2];
+		int[] nb_vivant = new int[NBJOUEUR];
 		nb_vivant[JOUEUR] = NBPERSONNAGE;
 		nb_vivant[IA] = NBPERSONNAGE;
 		
+		int boucle = 0;
+		System.out.println("oui ");
+		
 		/*on parcours équipe et on joue chacun des personnages*/
-		while(nb_vivant[JOUEUR]!=0 || nb_vivant[IA]!=0){
+		while(/*nb_vivant[JOUEUR]>0 || nb_vivant[IA]>0 || */boucle <= 20){
+			System.out.println("oui ");
 			for(indicePerso=NBPERSONNAGE-1;indicePerso >= 1;indicePerso--){
 				for(indiceJoueur=0;indiceJoueur <= NBJOUEUR-1;indiceJoueur++) {
+					
+					System.out.println("change perso ");
+					
+					System.out.println("ici on joue "+equipe[indicePerso][indiceJoueur].nomPersonnage());
+					
 					if(equipe[indicePerso][indiceJoueur].getEtat()!=MORT) {
+						
+						//System.out.println("on joue "+equipe[indicePerso][indiceJoueur].nomPersonnage());
+						
 						nb_vivant[indiceJoueur] += jouerPersoJoueur(jeu,equipe[indicePerso][indiceJoueur]);
+						nb_vivant[JOUEUR]-= 1;////////////
+						nb_vivant[IA]--;////////////
+						boucle++;
+						
+						if(nb_vivant[JOUEUR]>0 || nb_vivant[IA]>0 ) {
+							System.out.println("joueur = "+nb_vivant[JOUEUR]+" "+nb_vivant[IA]);
+						}
+						System.out.println("joueur = "+nb_vivant[JOUEUR]+" "+nb_vivant[IA]);
 					}
 				}
 			}
+			System.out.println("fin d'une session de jeux : boucle = "+boucle+" | joueur = "+nb_vivant[JOUEUR]+" "+nb_vivant[IA]);
+			repaint();	
 		}
 		
 	}
@@ -63,25 +158,48 @@ public class PanneauJeu extends JPanel implements IConfig {
 	public int jouerPersoJoueur(Carte jeu, Personnage perso) {
 		int i;
 		int resultat = 0;
-		int x=0;
-		int y=0;
 		Position positionDepart = perso.getPos();
+		int xd=positionDepart.getX();
+		int yd=positionDepart.getY();
 		Position positionCase = new Position(0,0); /////////////////
+		
+		int boucle = 0;////////////////////
 		
 		perso.setAttaque(1);
 		perso.setPm(perso.getVitesse());
 		
-		
-		while(perso.getAttaque()!=0 && perso.getPm() != 0) {
+		while(perso.getAttaque()!=0 && perso.getPm() != 0 && boucle != 2) {
+			//System.out.println("oui est dans la boucle perso");
 			/*on se place en attente d'un click sur la carte : selon la position, l'effet sera différent*/
+		/*	if(clickPosition()!=null) {
+				positionCase = clickPosition();
+			}*/
 			positionCase = clickPosition();
-			
-			/*le joueur a cliqué sur une case vide a cote du perso et perso.pm!=0*/
-			if(perso.getPm() !=0) {
-				jeu.Deplacement(positionDepart, positionCase);
+			/*on entre dans ce if seulement si on a cliqué sur une case*/
+			if(positionCase.getX()!=0 && positionCase.getY()!=0 && positionCase != null) {
+				int xa = positionCase.getX();
+				int ya = positionCase.getY();
+				/*si le joueur a cliqué sur la case où se trouve le perso, il se repose, ou passe son tour*/
+				if(xa==xd && ya==yd) {
+					if(perso.getAttaque()==0 || perso.getPm() != perso.getVitesse()) {
+						perso.repos();
+					}
+					else {
+						perso.passeTour();
+					}
+				}
+				/*le joueur a cliqué sur une case vide a cote du perso et perso.pm!=0*/
+				if(perso.getPm() !=0) {
+					jeu.Deplacement(positionDepart, positionCase);
+				}
 			}
+			
+			/*on remet la position case a 0 la suite de la boucle*/
+			positionCase.setX(0);
+			positionCase.setY(0);	
+			boucle++;///////////////
 		}
-		
+		System.out.println("perso fini");
 		return resultat;
 	}
 	
