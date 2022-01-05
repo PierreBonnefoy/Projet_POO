b450f1b954b1cb0c5e9a765c060c395b98e5e0cf
PierreBonnefoy <pierreeta@hotmail.fr>
2022-01-05 11:45:49 +0100

    Reste à implenter dans le projet

---
+++ Wargame/src/wargame/Dijkstra.java
@@ -0,0 +1,43 @@
+package wargame;
+
+public class Dijkstra {
+	
+	public void dijkstra(Carte map) {
+		
+		/*--- Initialisation du tableau de couleur && Initialisation du tableau des distance ---*/
+		boolean visiter[]= new boolean[map.taille];
+		int distance[]= new int[map.taille];
+		for(int i=0;i<map.taille;i++) {
+			distance[i]=Integer.MAX_VALUE;
+			visiter[i]=false;
+		}
+		distance[0]=0;
+		
+		for(int i=0;i<map.taille;i++) {
+			
+			int tmp=coutPlusFaible(distance,visiter);
+			visiter[tmp]=true;
+			
+			for(int j=0;j<map.taille;j++) {
+				if(!visiter[j] && map.carte[tmp][j].getNature()<=1 && distance[tmp]+map.carte[tmp][j].getNature()+1<distance[j]) {
+					distance[j]=distance[tmp]+map.carte[tmp][j].getNature()+1;
+				}
+			}
+		}
+		
+	}
+	
+	private int coutPlusFaible(int distance[],boolean visiter[]) {
+		
+		int minIdex = -1, min=Integer.MAX_VALUE;
+		
+		for(int i=0;i<distance.length;i++) {
+			if(!visiter[i] && distance[i]<min) {
+				min=distance[i];
+				minIdex=i;
+			}
+		
+		}
+		return minIdex;
+	}
+}
