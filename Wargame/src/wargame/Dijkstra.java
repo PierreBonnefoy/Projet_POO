d86dacae2350d50100d4e1dda3cdcfb0b896cf58
PierreBonnefoy <pierreeta@hotmail.fr>
2022-01-05 15:11:43 +0100

    à tester

--- Wargame/src/wargame/Dijkstra.java
+++ Wargame/src/wargame/Dijkstra.java
@@ -2,42 +2,50 @@ package wargame;
 
 public class Dijkstra {
 	
-	public void dijkstra(Carte map) {
+	public void dijkstra(Carte map,Position debut,Position fin) {
 		
 		/*--- Initialisation du tableau de couleur && Initialisation du tableau des distance ---*/
-		boolean visiter[]= new boolean[map.taille];
-		int distance[]= new int[map.taille];
+		boolean visiter[][]= new boolean[map.taille][map.taille];
+		int distance[][]= new int[map.taille][map.taille];
 		for(int i=0;i<map.taille;i++) {
-			distance[i]=Integer.MAX_VALUE;
-			visiter[i]=false;
-		}
-		distance[0]=0;
-		
-		for(int i=0;i<map.taille;i++) {
-			
-			int tmp=coutPlusFaible(distance,visiter);
-			visiter[tmp]=true;
-			
 			for(int j=0;j<map.taille;j++) {
-				if(!visiter[j] && map.carte[tmp][j].getNature()<=1 && distance[tmp]+map.carte[tmp][j].getNature()+1<distance[j]) {
-					distance[j]=distance[tmp]+map.carte[tmp][j].getNature()+1;
+				distance[i][j]=Integer.MAX_VALUE;
+				if(i==0 && Math.floorMod(j, 2)==0) {
+					visiter[i][j]=false;
 				}
 			}
 		}
+		distance[debut.getX()][debut.getY()]=0;
+		Position tmp;
+		
+		do {
+			/*--- Détermination de la position suivante ---*/
+			tmp=coutPlusFaible(map,distance,visiter,debut);
+			/*--- On la marque visiter --*/
+			visiter[tmp.getX()][tmp.getY()]=true;
+			/*--- Verif si de cout est rentable ---*/
+			if(!visiter[tmp.getX()][tmp.getY()] && map.carte[tmp.getX()][tmp.getY()].getNature()<=1 && distance[tmp.getX()][tmp.getY()]+map.carte[tmp.getX()][tmp.getY()].getNature()+1<distance[tmp.getX()][tmp.getY()]) {
+				distance[tmp.getX()][tmp.getY()]=distance[tmp.getX()][tmp.getY()]+map.carte[tmp.getX()][tmp.getY()].getNature()+1;
+			}
+			/*--- Condition de fin (arrive) ---*/
+		}while(tmp.getX()!=fin.getX() && tmp.getX()!=fin.getX() && tmp.getX()!=fin.getX() && tmp.getX()!=fin.getX() );
 		
 	}
 	
-	private int coutPlusFaible(int distance[],boolean visiter[]) {
+	private Position coutPlusFaible(Carte map,int distance[][],boolean visiter[][],Position debut) {
 		/*--- Retourne l'index qui a le cout de deplacement le plus faible ---*/
-		int minIdex = -1, min=Integer.MAX_VALUE;
+		Position minIdex = new Position(0,0),tmp[]=map.PositionPossible(debut);
+		int min=Integer.MAX_VALUE;
 		
-		for(int i=0;i<distance.length;i++) {
-			/*--- Verif que le sommet n'a pas ete visiter et que son cout est plus faible ---*/
-			if(!visiter[i] && distance[i]<min) {
-				min=distance[i];
-				minIdex=i;
+		for(int i=0;i<tmp.length;i++) {
+			/*--- Verif que le sommet n'a pas ete visiter et que son cout est plus faible que les autres positions ---*/
+			if(tmp[i]!=null) {
+				if(!visiter[tmp[i].getX()][tmp[i].getY()] && distance[tmp[i].getX()][tmp[i].getY()]<min) {
+					min=distance[tmp[i].getX()][tmp[i].getY()];
+					minIdex.setX(tmp[i].getX());
+					minIdex.setY(tmp[i].getY());
+				}
 			}
-		
 		}
 		return minIdex;
 	}
