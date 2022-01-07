0cfdc696bdfd05f1b787468081369aa19fec74ce
2703-web <garnier.soan@gmail.com>
2022-01-07 21:31:59 +0100

    menu aide

--- Wargame/src/wargame/MenuAide.java
+++ Wargame/src/wargame/MenuAide.java
@@ -14,13 +14,21 @@ public class MenuAide extends JPanel implements IConfig{
 				"<br>\r\n" + 
 				"Le Jouer va incarner une équipe de monstre du Donjon qui doit repousser l’invasion des Démons. \r\n" + 
 				"\r\n" + 
-				"<h2>Rêgles du jeux</h2>\r\n" + 
+				"<h2>____Rêgles du jeux</h2>\r\n" + 
 				"Le Joueur joue une équipe de 10 Monstres qui jouent chacun selon un ordre d’Initiative : celui qui à l’Initiative la plus élevée joue en premier. <br>\r\n" + 
 				"Les Ennemis jouent une équipe semblable, et les créatures de chaque équipe jouent à tour de rôle.<br>\r\n" + 
 				"Lors de son tour de jeux, un Personnage agit selon l’endroit où l’on clique avec la souris : \r\n" + 
 				"<ul><li>Se déplacer (cliquez sur une case vide accessible)</li>\r\n" + 
 				"<li>Attaquer (cliquez sur une case occupée (par un ennemi)</li><li>Passer son tour (cliquez sur le personnage</li></ul>"
-				+ "</html>");
+				+ "Chaque personnage possède un némésis contre lequel il inflige davantage de dégâts.<br>"
+				+ "En combattant, chaque personnage gagne de l'expérience.<br>"
+				+ "Gagner un niveau augmente la puissance du personnage.<br>"+
+				"<h2>____Carte</h2>"
+				+ "La carte est couverte par un brouillard de guerre : explorez-là pour trouver vos ennemis !<br>"
+				+ "Chaque case possède un terrain spécifique :<br>"
+				+ "<ul><li>forêt & sol : déplacement libre.</li>"
+				+ "<li>lave & montagne : déplacement impossible.</li></ul>"
+				+"</html>");
 		JButton retour = new JButton ("retour");
 		this.setBackground(new Color(127, 127, 127));
 		Explication.setMaximumSize(new Dimension(LARGEUR_FENETRE,HAUTEUR_FENETRE));
